dfe5e12f3fb756ab5e5df148764529866fa06b52
David Higgins <david14higgins@gmail.com>
2024-11-22 12:00:54 +0000

    impl AA CLI flag

--- src/main.cpp
+++ src/main.cpp
@@ -12,22 +12,27 @@ int main(int argc, char* argv[]) {
             std::cout << "Please provide a path to a scene file." << std::endl;
             std::cout << "Options:" << std::endl;
             std::cout << "  --bvh    Enable BVH acceleration structure" << std::endl;
+            std::cout << "  --useAA  Enable anti-aliasing" << std::endl;
             return 0;
         }
 
         std::string filename = argv[1];
 
         bool useBVH = false;
+        bool useAntiAliasing = false;
+        int samplesPerPixel = 8;
+
         for (int i = 2; i < argc; i++) {
-            if (std::string(argv[i]) == "--bvh") {
+            if (std::string(argv[i]) == "--useBVH") {
                 useBVH = true;
-                break;
+            }
+            if (std::string(argv[i]) == "--useAA") {
+                useAntiAliasing = true;
             }
         }
         std::cout << (useBVH ? "BVH acceleration enabled" : "BVH acceleration disabled") << std::endl;
-
-        bool useAntiAliasing = true;
-        int samplesPerPixel = 16;
+        std::cout << (useAntiAliasing ? "Anti-aliasing enabled" : "Anti-aliasing disabled") << std::endl;
+        
 
         Renderer renderer = IO::loadRenderer(filename, useBVH, useAntiAliasing, samplesPerPixel);
         auto start = std::chrono::high_resolution_clock::now();
